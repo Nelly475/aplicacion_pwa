29087d98869fb415bef66ec4288a03a22744caaa
Nelly475 <71159638+Nelly475@users.noreply.github.com>
2022-09-25 16:25:16 -0500

    fetch4

---
+++ fetch-4.js
@@ -0,0 +1,11 @@
+//https://reqres.in/api/users
+
+let img = document.querySelector('img');
+
+fetch('utt.png')
+.then(resp => resp.blob())
+.then (imagen => {
+
+    var imgPath = URL.createObjectURL(imagen);
+    img.src = imgPath;
+});
