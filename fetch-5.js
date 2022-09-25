3c9cdde4999dbd0576db003dce805f1973499c0d
Nelly475 <71159638+Nelly475@users.noreply.github.com>
2022-09-25 16:50:00 -0500

    fetch5

---
+++ fetch-5.js
@@ -0,0 +1,17 @@
+
+
+fetch('https://reqres.in/api/users/10000')
+    .then(resp => {
+        if(resp.ok){
+            return resp-json();
+        }
+        else{
+            throw new Error('No esxiste este usuario')
+        }
+    })
+
+    .then(console.log)
+    .catch(error => {
+        console.log('Error en la peticion');
+        console.log(error);
+    });
